d022763927e6f060c4c8fc98b2f85e0281486055
Samuel <samuel.dedios@urosario.edu.co>
2025-11-20 11:53:52 -0500

    Correccion del metodo del disparo.

--- problemas_valor_inicial/metodo_disparo.m
+++ problemas_valor_inicial/metodo_disparo.m
@@ -24,22 +24,8 @@ function [ts,yts] = metodo_disparo(f, a, b, alpha, beta, n)
     yb1 = sol1(end, 2);
     yb2 = sol2(end, 2);
 
-    tol = 1e-6;
-    iter = 0; maxiter = 20;
+    zr = z2 - (yb2 - beta)*(z2 - z1)/(yb2 - yb1);
 
-    while abs(yb2 - beta) > tol && iter < maxiter
-        iter = iter + 1;
-
-        zr = z2 - (yb2 - beta)*(z2 - z1)/(yb2 - yb1);
-
-        solr = sistemas_rk4(F, [alpha, zr], a, b, n);
-        ybr = solr(end, 2);
-
-        %actualiza valores
-        z1 = z2; yb1 = yb2;
-        z2 = zr; yb2 = ybr;
-    end
-
-    ts  = solr(:, 1);
-    yts = solr(:, 2);
+    
+    [ts, yts] = sistemas_rk4(F, [alpha, zr], a, b, n);
 end
